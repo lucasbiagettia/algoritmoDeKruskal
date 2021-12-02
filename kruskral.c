8ff2239bb80a9805b8bcbf36ecf22efb04c84a45
Lucas Biagetti <lucasbiagetti@gmail.com>
2021-12-02 12:45:10 -0300

    Add files via upload

--- kruskral.c
+++ kruskral.c
@@ -4,59 +4,59 @@
 #include <unistd.h>
 #include "kruskral.h"
 
+
 int main (){
 
     int M_Costos [VERTICES+1][VERTICES+1];
-    rama* grafo = NULL;
-    rama* arbol = NULL;
+    rama* grafo = NULL;     //Puntero a cabeza de lista enlazada en la que se guardará el grafo
+    rama* arbol = NULL;     //Puntero a cabeza de lista enlazada en la que se guardará el arbol
     conjunto_CE componentes;
+
 
     srand(getpid());
     int opcion = 0;
-
     do {
     	printf("Ingrese 1 para ingresar los valores manualmente o 2 para ingresar los valores de forma aleatoria: ");
         fflush(stdin);
         scanf ("%d",&opcion);
     } while ((opcion != 1) && (opcion !=2));
-
-
     for (int i=1; i <= VERTICES; i++){
-	for ( int j= i+1; j <= VERTICES; j++){
+        for ( int j= i+1; j <= VERTICES; j++){
             printf ("Ingrese costo de lado entre vertices %d y %d:" ,i ,j);
             fflush(stdin);
             if (opcion == 1){
-	    	scanf ("%d", &M_Costos[i][j]);
-	    } else if (opcion == 2) {
-		M_Costos[i][j] = (rand()%50)+1;
-            	printf ("%d \n", M_Costos[i][j]);
-	    }
-        }
-    }
-
-    for (int i=1; i <= VERTICES ; i ++){ // la mitad inf. de diagonal de matriz
-        for ( int j = i +1; j <= VERTICES ; j ++){
-            if (M_Costos [i][j] !=0){
-                inserta (i, j, M_Costos [i][j], &grafo); // inserto en cola prior.
+                scanf ("%d", &M_Costos[i][j]);
+            } else if (opcion == 2) {
+                M_Costos[i][j] = (rand()%50)+1;
+                printf ("%d \n", M_Costos[i][j]);
             }
         }
     }
 
-    for (int i=1; i<=VERTICES; i++){
-        inicial (i, i, &componentes);
+
+    for (int i=1; i <= VERTICES ; i ++){                    // la mitad inf. de diagonal de matriz
+        for ( int j = i +1; j <= VERTICES ; j ++){
+            if (M_Costos [i][j] !=0){
+                inserta (i, j, M_Costos [i][j], &grafo);    // inserto en cola prior.
+            }
+        }
     }
 
-    imprimeCE(&componentes);
 
+    printf ("\n");
+    printf ("El grafo ingresado está formado por las siguientes aristas");
     lista (grafo);
     printf("----\n");
 
-
     kruskal(grafo, &arbol, &componentes);
-    printf("Algoritmo de Kruskral finalizado\n");
+    printf("\n");
+    printf("Algoritmo de Kruskral finalizado\n\n");
 
+    printf ("El arbol abarcador de costo m\241nimo est\240 formado por las siguientes aristas:\n");
     lista(arbol);
 
     return 0;
 
 }
+
+
--- kruskral.h
+++ kruskral.h
@@ -30,24 +30,23 @@ typedef struct _CONJUNTO_CE{
     encabezado encabezamientos_conjunto [VERTICES+1];
 }conjunto_CE;
 
-void inicial            (tipo_nombre, tipo_elemento, conjunto_CE*);
-void combina            (tipo_nombre, tipo_nombre, conjunto_CE*);
-tipo_nombre encuentra   (int ,conjunto_CE*);
-void kruskal            (rama*, rama**, conjunto_CE*);
-void inserta            (int ,int ,int ,rama**);                    //Hecha. Agrega al principio de una lista enlazada
-void sacar_min       (rama**);
+void inicial            (tipo_nombre, tipo_elemento, conjunto_CE*); //Inicializa los valores del conjunto Combina Encuentra
+void combina            (tipo_nombre, tipo_nombre, conjunto_CE*);   //Combina dos conjuntos en uno (previamente se debe verificar con encuentra
+tipo_nombre encuentra   (int ,conjunto_CE*);                        //Devuelve el conjunto al que pertenecen dos elementos
+void kruskal            (rama*, rama**, conjunto_CE*);              //Realiza el algoritmo de Kruskal inicializando con inicia, e iterando con combina-encuentra
+void inserta            (int ,int ,int ,rama**);                    //Agrega una arista ordenada según su peso
+void sacar_min          (rama**);                                   //Elimina el primer elemento de una lista enlazada.
 void lista              (rama*);                                    //Imprime todo el grafo
 void imprimeCE          (conjunto_CE*);                             //IMPRIME LA TABLA COMBINA ENCUENTRA
+void imprimeEnTabla     (rama*);
 
 void imprimeCE (conjunto_CE* C){
-    printf("Encabezamiento conjuntos --------------------     nombres \n");
-    printf("     Cuenta || Primer el --------------------    NombreC || Sig\n");
+    printf("Encabezamiento conjuntos    --------------------           Nombres \n");
+    printf("       Cuenta | 1er elemento--------------------    Nombre Conj.| Siguiente el.\n");
 
     for (int i=1; i<=VERTICES; i++){
-        printf ("|%02d |   |%02d || %02d |   ----------------------   |%02d || %02d || %02d |\n", i, C->encabezamientos_conjunto[i].cuenta, C->encabezamientos_conjunto[i].primer_elemento,i, C->nombres[i].nombre_conjunto ,C->nombres[i].siguiente_elemento );
-
+        printf ("|%2d |   %2d    |   %2d    |   --------------------  |%2d |   %2d    |   %2d    |\n", i, C->encabezamientos_conjunto[i].cuenta, C->encabezamientos_conjunto[i].primer_elemento,i, C->nombres[i].nombre_conjunto ,C->nombres[i].siguiente_elemento );
     }
-
 }
 
 void inicial (tipo_nombre A, tipo_elemento x, conjunto_CE * C){
@@ -58,29 +57,32 @@ void inicial (tipo_nombre A, tipo_elemento x, conjunto_CE * C){
 }
 
 void kruskal (rama* cabeza, rama** arbol, conjunto_CE* componentes){
-    //INICIALIZA LA TABLA COMBINA_ENCUENTRA
-    for (int i=1; i<=VERTICES; i++){
+    for (int i=1; i<=VERTICES; i++){                //INICIALIZA LA TABLA COMBINA_ENCUENTRA
         inicial (i, i, componentes);
     }
 
-    //SI HAY ALGUN ELEMENTO EN LA LISTA DE PRIORIDAD
+    int it = 1; //Esta variable sólo sirve para imprimir el número de iteración.
+
+    //Si hay algún elemento en la lista de prioridad
     while (cabeza != NULL){
 
-        arista a = cabeza->a;               //Arista toma el valor del primer elemento
-        sacar_min(&cabeza);                 //Elimina el primer elemento (la funcion ahora es void) con esto cambia el puntero
+        arista a = cabeza->a;                       //Arista toma el valor del primer elemento
+        sacar_min(&cabeza);                         //Elimina el primer elemento (la funcion ahora es void) con esto cambia el puntero
 
-        tipo_nombre comp_u = a.u;           //comp_u y comp_v toman los valores de los vertices de la arista que estamos trabajando.
+        tipo_nombre comp_u = a.u;                   //comp_u y comp_v toman los valores de los vertices de la arista que estamos trabajando.
         tipo_nombre comp_v = a.v;
         comp_u = encuentra(comp_u, componentes);    //comp_u y comp_v toman el valor del conjunto al que pertenecen
         comp_v = encuentra(comp_v, componentes);
 
-        imprimeCE(componentes);
+        printf ("\nIteraci\242n %d\n",it);
+        it++;
 
-        printf ("Toma la arista %d %d, que tiene costo %d...\n",a.u,a.v,a.costo);
+        imprimeCE(componentes);                         //Imprime la tabla CE
+        printf ("Toma la arista %d %d, costo %d:\n",a.u,a.v,a.costo);
 
-        if (comp_u != comp_v){                    //si pertenecen a otro conjunto los combina()
+        if (comp_u != comp_v){                          //si pertenecen a conjuntos distintos las une con combina()
     	    combina(comp_u, comp_v, componentes);
-            inserta(a.u, a.v, a.costo, arbol); //usa la función inserta pero ahora para armar otra cola con las aristas del arbol
+            inserta(a.u, a.v, a.costo, arbol);          //usa la función inserta pero ahora para armar otra cola con las aristas del arbol
             printf ("Inserta la arista %d %d: de los conjuntos %d y %d\n",a.u,a.v, comp_u, comp_v);
         }
 
@@ -90,30 +92,32 @@ void kruskal (rama* cabeza, rama** arbol, conjunto_CE* componentes){
 
 
 void combina (tipo_nombre A, tipo_nombre B, conjunto_CE * C){
-	//Funcion combina conjunto A con B, modifica cuenta de cada conjunto
 	int i;
-	if (((C->encabezamientos_conjunto[A]).cuenta) > ((C->encabezamientos_conjunto[B]).cuenta)){
+
+	if (((C->encabezamientos_conjunto[A]).cuenta) > ((C->encabezamientos_conjunto[B]).cuenta)){  //Si la cantidad de elementos de A es mayor a la de B
 		i = (C->encabezamientos_conjunto[B]).primer_elemento;
-		while ((C->nombres[i]).siguiente_elemento != 0 ) {
+		while ((C->nombres[i]).siguiente_elemento != 0 ) {  //itera hasta que no haya siguiente elemento, actualizando el nombre del conjunto en la tabla Nombre
 			(C->nombres[i]).nombre_conjunto = A;
 			i = (C->nombres[i]).siguiente_elemento;
 		}
+
+                                                            //Actualiza el resto de los valoes
 		(C->nombres[i]).nombre_conjunto = A;
 		(C->nombres[i]).siguiente_elemento = (C->encabezamientos_conjunto[A]).primer_elemento;
 		(C->encabezamientos_conjunto[A]).primer_elemento = (C->encabezamientos_conjunto[B]).primer_elemento;
 		(C->encabezamientos_conjunto[A]).cuenta = (C->encabezamientos_conjunto[A]).cuenta + (C->encabezamientos_conjunto[B]).cuenta;
 		(C->encabezamientos_conjunto[B]).primer_elemento = 0;
 		(C->encabezamientos_conjunto[B]).cuenta = 0;
-	} else {
-                i = (C->encabezamientos_conjunto[A]).primer_elemento;
-                while ((C->nombres[i]).siguiente_elemento != 0 ) {
-                        (C->nombres[i]).nombre_conjunto = B;
-                        i = (C->nombres[i]).siguiente_elemento;
-                }
-                (C->nombres[i]).nombre_conjunto = B;
+	}else{
+        i = (C->encabezamientos_conjunto[A]).primer_elemento;//Idem arriba pero para el caso B >= A
+        while ((C->nombres[i]).siguiente_elemento != 0 ) {
+            (C->nombres[i]).nombre_conjunto = B;
+            i = (C->nombres[i]).siguiente_elemento;
+            }
+        (C->nombres[i]).nombre_conjunto = B ;
 		(C->nombres[i]).siguiente_elemento = (C->encabezamientos_conjunto[B]).primer_elemento;
-                (C->encabezamientos_conjunto[B]).primer_elemento = (C->encabezamientos_conjunto[A]).primer_elemento;
-                (C->encabezamientos_conjunto[B]).cuenta = (C->encabezamientos_conjunto[B]).cuenta + (C->encabezamientos_conjunto[A]).cuenta;
+        (C->encabezamientos_conjunto[B]).primer_elemento = (C->encabezamientos_conjunto[A]).primer_elemento;
+        (C->encabezamientos_conjunto[B]).cuenta = (C->encabezamientos_conjunto[B]).cuenta + (C->encabezamientos_conjunto[A]).cuenta;
 		(C->encabezamientos_conjunto[A]).primer_elemento = 0;
 		(C->encabezamientos_conjunto[A]).cuenta = 0;
 	}
@@ -123,18 +127,19 @@ tipo_nombre encuentra (int x, conjunto_CE * C){
 	return ((*C).nombres[x].nombre_conjunto);
 	}
 
-void inserta(int i, int j, int peso, rama** cabeza){
-    rama* nuevo = (rama*)malloc(sizeof(rama));
+void inserta(int i, int j, int peso, rama** cabeza){    //Inserta arista en la cola de prioridad según su costo
+    rama* nuevo = (rama*)malloc(sizeof(rama));          //Reserva memoria
 
     if (nuevo==NULL){
         printf("Error al reservar memorioa\n");
         free(nuevo);
     }
-
+                                                        //Inicializa los valores de la arista (vértices y costo)
     nuevo->a.costo = peso;
     nuevo->a.u = i;
     nuevo->a.v = j;
 
+                                                        //Busca el lugar en dónde insertar. Se analiza el caso particular en la que no hay primer elemento.
     if ((*cabeza) == NULL){
         nuevo->sig = NULL;
         (*cabeza) = nuevo;
@@ -157,21 +162,26 @@ void inserta(int i, int j, int peso, rama** cabeza){
     }
 }
 
-void lista (rama* cabeza){
-    //imprime las ramas
+void lista (rama* cabeza){                              //Imprime todos los elementos de un grafo a partir de una lista enlazada
     if (cabeza==NULL){
         printf ("No hay ninguna arista en el grafo");
     }
     else{
+
         while (cabeza != NULL){
-            printf ("Esta rama une %d con %d y su costo es %d\n", cabeza->a.u, cabeza->a.v, cabeza->a.costo);
+            printf ("Arista %d,%d de costo %d\n", cabeza->a.u, cabeza->a.v, cabeza->a.costo);
             cabeza=cabeza->sig;
         }
     }
 }
 
-void sacar_min (rama** cabeza){
+void sacar_min (rama** cabeza){   //Elimina el primer elemento de una lista enlazada. En este caso la que contiene al grafo completo.
     rama* aux = *cabeza;
     *cabeza = (*cabeza)->sig;
     free(aux);
 }
+
+
+
+
+
